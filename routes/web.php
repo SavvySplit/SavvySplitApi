1206c3dc5ff6dd93d5fcd407bdd2a12ab2b37991
berberacrobat <berberacrobat@gmail.com>
2025-04-26 12:36:15 -0700

    return app is wokring

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-   // return "App is working ....";
+    return "App is working ....";
     return Inertia::render('welcome');
 })->name('home');
 
